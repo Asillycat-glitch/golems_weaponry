fe4f325f20785fc590f81f5fa7e9ec177fee87e1
Asillycat-glitch <2440464323@qq.com>
2026-05-23 03:21:33 +0800

    Delete src/main directory

--- src/main/java/a_silly_cat/modulargolems_weaponry/capability/IProficiencyCounter.java
+++
@@ -1,8 +0,0 @@
-// 文件路径: capability/IProficiencyCounter.java
-package a_silly_cat.modulargolems_weaponry.capability;
-
-public interface IProficiencyCounter {
-    // 新方法
-    void addPoints(int amount);
-    int getPoints();
-}
