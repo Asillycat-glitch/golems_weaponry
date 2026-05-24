ce750f1f683a839de337ec57c56dbe88d7587dbd
Asillycat-glitch <2440464323@qq.com>
2026-05-24 14:04:58 +0800

    Delete src/main directory

--- src/main/java/a_silly_cat/modulargolems_weaponry/capability/ProficiencyCounterProvider.java
+++
@@ -1,9 +0,0 @@
-package a_silly_cat.modulargolems_weaponry.capability;
-
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.CapabilityToken;
-
-public class ProficiencyCounterProvider {
-    public static final Capability<IProficiencyCounter> PLAYER_COUNTER_CAP = CapabilityManager.get(new CapabilityToken<>(){});
-}
